c023eaa1870cb0a0a7707c9ea1352cca4dbe0623
Daniel-L10N <alejandrolazolemus@gmail.com>
2023-07-26 02:10:15 -0400

    agg config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./Public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        negro: '#000',
+      }
+    },
   },
   plugins: [],
 }
